56449e2dc8e7e8d63dc99ac84aa1b83d03dcac68
aleks <alexandra.kojic@gmail.com>
2024-04-17 00:17:09 +0200

    k8s

--- jenkins-server/backend.tf
+++ jenkins-server/backend.tf
@@ -3,8 +3,6 @@ terraform {
     bucket         = "aws-eks-tf"
     region         = "eu-west-3"
     key            = "server/terraform.tfstate"
-    dynamodb_table = "lock"
-    encrypt        = true
   }
   required_version = ">=0.13.0"
   required_providers {
